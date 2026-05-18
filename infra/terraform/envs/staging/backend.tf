203b4c50876bb4f10d10a96036ae72d53715cd51
Marten Schlueter <hi.marten@proton.me>
2026-05-18 18:06:41 +0200

    Drop envs/staging after destroying the partial bring-up state

--- infra/terraform/envs/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "bioagents-tf-state"
-    key          = "envs/staging/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
